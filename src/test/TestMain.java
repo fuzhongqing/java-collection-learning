749c20c7ea3df561fb887327c9a3342c7ae9b926
fuzho <fuzho@DESKTOP-GMN76HS>
2016-05-11 18:32:25 +0800

    util.Vector 完成基本接口 test.TestMain vector的测试

---
+++ src/test/TestMain.java
@@ -0,0 +1,35 @@
+package test;
+
+import util.Vector;
+import static org.junit.Assert.*;
+public class TestMain {
+
+	static void testVector() {
+		Vector<Integer> v = new Vector<>(0,0);
+		assertEquals(0, v.vsz());
+		
+		v.add(3);v.add(1);v.add(4);v.add(1);v.add(5);
+		
+		assertEquals(5, v.size());
+		assertEquals(8, v.vsz());
+		assertEquals(v.toString(),"[3, 1, 4, 1, 5]");
+		
+		Vector<Integer> copyOfv = new Vector<>(v);
+		
+		assertEquals(5, copyOfv.vsz());
+		assertArrayEquals(v.toArray(), copyOfv.toArray());
+		
+		v.dropUselessSpace();
+		assertEquals(5, v.vsz());
+		
+		v.setSize(10);
+		assertEquals(10, v.vsz());
+		assertEquals(5, v.size());
+		
+		System.out.println("Vector : all testcases passed!");
+	}
+	public static void main(String[] args) {
+		testVector();
+	}
+
+}
