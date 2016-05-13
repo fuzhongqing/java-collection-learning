1bbd51eb0f979a58c48b4d6d24f6fec9094aa358
fuzhongqing <fuzhongqing1995@gmail.com>
2016-05-13 15:46:45 +0800

    补全队列方法

--- src/util/Queue.java
+++ src/util/Queue.java
@@ -1,5 +1,23 @@
 package util;
 
+@SuppressWarnings("rawtypes")
 public class Queue<E> extends Vector {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5630260307698867503L;
+
+	@SuppressWarnings("unchecked")
+	public void push_back(E e) {
+		add(e);
+	}
+	@SuppressWarnings("unchecked")
+	public E front() {
+		return (E) firstElement();
+	};
+	public void pop() {
+		removeElementAt(0);
+	};
 	
 }
